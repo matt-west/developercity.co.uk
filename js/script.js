849cc8ee4b043e4f408451e48f5151672141a79c
Matt West <matt.west@developercity.co.uk>
2012-04-18 14:29:03 +0100

    Moved social JS code to script.js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,10 @@ $(document).ready(function() {
 		$('#development').css('height', window.innerHeight + 'px');
 	}
 
+	if ($('body').hasClass('social')) {
+		getSocial();
+	}
+
 	headerAdditions = '<div id="menu-side"><a href="#">&#171;</a></div><div id="menu-popout"><ul class="clearfix"><li><a href="about.html" title="About"><div class="page-title">About</div><div class="page-description">Who We Are</div></a></li><li><a href="design.html" title="Web Design"><div class="page-title">Design</div><div class="page-description">Crafting Pixels</div></a></li><li><a href="development.html" title="Web Development"><div class="page-title">Development</div><div class="page-description">Mining Data</div></a></li><li><a href="projects.html" title="Projects"><div class="page-title">Projects</div><div class="page-description">Our Work</div></a></li><li><a href="social.html" title="Social Stream"><div class="page-title">Social</div><div class="page-description">Our Noise</div></a></li><li><a href="contact.html" title="Contact"><div class="page-title">Contact</div><div class="page-description">Say Hello!</div></a></li></ul></div>';
 
 	$("header").append(headerAdditions);
@@ -62,3 +66,15 @@ $(document).ready(function() {
 	});
 
 });
+
+function getSocial() {
+  $('#twitter-posts').twitter({from: 'MattAntWest', retweets: false, limit : 8, avatar : false});
+
+  $("#instagram-photos").instagram({
+    userId: '10798725',
+    //hash: 'food',
+    clientId: '6b108482fa9944a694a6b1d8807d95a6',
+    accessToken: '10798725.6b10848.eaac2a39c14d4e8e8a5d1b03a3a928ed',
+    show: '12'
+  });
+}
