3b37c6d41ab628322057faaf72c89da73c3624ce
Matt West <matt.west@developercity.co.uk>
2012-08-16 13:49:59 +0100

    Updated publish

---
+++ publish/templates/footer.php
@@ -0,0 +1,18 @@
+<footer class=clearfix>
+    <p>Explore the Unknowable</p>
+    <small>&copy; 2010-2012 Developer City. All Rights Reserved.</small>
+  </footer>
+
+
+  
+
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script>window.jQuery||document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>');</script>
+
+  <script defer src='js/08e10df.js'></script>
+
+  
+  <script>var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]];(function(e,a){var c=e.createElement(a),b=e.getElementsByTagName(a)[0];c.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";b.parentNode.insertBefore(c,b)}(document,"script"));</script>
+</body>
+</html>
