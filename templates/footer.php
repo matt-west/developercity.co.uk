cab1e365763e84dad804498896d469f437ff0dfb
Matt West <matt.west@developercity.co.uk>
2012-09-30 11:39:28 +0100

    Added Developer City as publisher.

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
   <!-- Footer -->
   <footer class="clearfix">
     <p>Explore the Unknowable</p>
-    <small>&copy; 2010-2012 Developer City &amp; <a href="https://plus.google.com/103780700566333678292?rel=author" title="Matt West" rel="author" target="_blank">Matt West</a>. All Rights Reserved.</small>
+    <small>&copy; 2010-2012 <a href="https://plus.google.com/101559871987141006488" rel="publisher">Developer City</a> &amp; <a href="https://plus.google.com/103780700566333678292?rel=author" title="Matt West" rel="author" target="_blank">Matt West</a>. All Rights Reserved.</small>
   </footer>
 
 
