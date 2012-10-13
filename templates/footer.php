f086d8e5ff69dff731be1798c7c59d7db8bba263
Matt West <matt.west@developercity.co.uk>
2012-10-13 11:59:26 +0100

    Adding GoSquared Analytics Code

--- templates/footer.php
+++ templates/footer.php
@@ -17,6 +17,24 @@
   <!-- end scripts -->
 
   <script type="text/javascript">
+    var GoSquared = {};
+    GoSquared.acct = "GSN-750505-N";
+    (function(w){
+      function gs(){
+        w._gstc_lt = +new Date;
+        var d = document, g = d.createElement("script");
+        g.type = "text/javascript";
+        g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+        var s = d.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(g, s);
+      }
+      w.addEventListener ?
+        w.addEventListener("load", gs, false) :
+        w.attachEvent("onload", gs);
+    })(window);
+  </script>
+
+  <script type="text/javascript">
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-12756669-1']);
     _gaq.push(['_trackPageview']);
